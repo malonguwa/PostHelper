acdd547a1292faf52b432d432867665f0c94eadf
LONG MA <longma@longs-macbook-pro.local>
2016-11-15 19:38:11 +0800

    first commit - test

---
+++ PostHelper/PostHelper/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  PostHelper
+//
+//  Created by LONG MA on 15/11/16.
+//  Copyright © 2016 HnA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
