61117246757f778c606718f010207b84f13ba9b6
LONG MA <longma@LONGs-MacBook-Pro.local>
2017-01-10 21:37:45 +0800

    Add new Manager Classes for future use

---
+++ PostHelper/PostHelper/Home/HATwitterManager.swift
@@ -0,0 +1,13 @@
+//
+//  HATwitterManager.swift
+//  PostHelper
+//
+//  Created by LONG MA on 10/1/17.
+//  Copyright © 2017 HnA. All rights reserved.
+//
+
+import UIKit
+
+class HATwitterManager: NSObject {
+
+}
