e7307204c1e12cfc834670485f4f38b29cc58a94
LONG MA <longma@LONGs-MacBook-Pro.local>
2017-01-10 21:35:31 +0800

    1. Fix Facebook Login AccessToken Bug 2. Twitter Login and Logout

--- PostHelper/PostHelper/Login/HALoginVC.swift
+++ PostHelper/PostHelper/Login/HALoginVC.swift
@@ -10,19 +10,34 @@ import UIKit
 import FacebookLogin
 import FacebookCore
 import SafariServices
+import FBSDKCoreKit
+import TwitterKit
 
 class HALoginVC: UIViewController, SFSafariViewControllerDelegate {
 
     // MARK: Property
     /// Btn : Add facebook account
     @IBOutlet weak var addFBAccountBtn: UIButton!
+    @IBOutlet weak var addTWAccountBtn: UIButton!
+    @IBOutlet weak var FBDisconnectBtn: UIButton!
+    @IBOutlet weak var TWDisconnectBtn: UIButton!
     
     // MARK: Function
     override func viewDidLoad() {
         super.viewDidLoad()
+        NotificationCenter.default.addObserver(self, selector: #selector(HALoginVC.HAFacebookCheckLogin), name: NSNotification.Name.FBSDKAccessTokenDidChange, object: nil)
+
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    func HAFacebookCheckLogin() {
+        if AccessToken.current != nil {// User is already logged in, do work such as go to next view controller.
+            print("Connected to facebook already")
+        } else {
+            print("Not connect to facebook")
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
@@ -34,7 +49,7 @@ class HALoginVC: UIViewController, SFSafariViewControllerDelegate {
         
         let loginManager = LoginManager()
         loginManager.loginBehavior = .native
-        loginManager.logIn([.publicProfile, ReadPermission.custom("user_photos"), ReadPermission.custom("user_videos")], viewController: self) { loginResult in
+        loginManager.logIn([.publishActions], viewController: self) { loginResult in
             switch loginResult {
             case .failed(let error):
                 print(error)
@@ -42,21 +57,52 @@ class HALoginVC: UIViewController, SFSafariViewControllerDelegate {
                 print("User cancelled login.")
             case .success(let grantedPermissions, let declinedPermissions, let accessToken):
                 
-                print("Logged in with read permission!, \n grantedPermissions: \(grantedPermissions), \n declinedPermissions: \(declinedPermissions),\n accessToken: \(accessToken)")
-                loginManager.logIn([.publishActions], viewController: self) { loginResult in
-                    switch loginResult {
-                    case .failed(let error):
-                        print(error)
-                    case .cancelled:
-                        print("User cancelled login.")
-                    case .success(let grantedPermissions, let declinedPermissions, let accessToken):
-                        
-                        print("Logged in with read permission!, \n grantedPermissions: \(grantedPermissions), \n declinedPermissions: \(declinedPermissions),\n accessToken: \(accessToken)")
-                    }
-                    
-                }
+                print("Logged in with write permission!, \n\n grantedPermissions: \(grantedPermissions), \n\n declinedPermissions: \(declinedPermissions),\n\n accessToken: \(accessToken)")
+        }
+    }
+}
+    
+    /// addTWAccountBtn event
+    @IBAction func addTWAccountClick(_ sender: Any) {
+        Twitter.sharedInstance().logIn { (session, error) in
+            if session != nil {
+                print("signed in as \(session?.userName)")
+            } else {
+                print("error : \(error?.localizedDescription)")
             }
         }
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+//        loginManager.logIn([.publicProfile, ReadPermission.custom("user_photos"), ReadPermission.custom("user_videos")], viewController: self) { loginResult in
+//            switch loginResult {
+//            case .failed(let error):
+//                print(error)
+//            case .cancelled:
+//                print("User cancelled login.")
+//            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+//
+//                print("Logged in with read permission!, \n grantedPermissions: \(grantedPermissions), \n declinedPermissions: \(declinedPermissions),\n accessToken: \(accessToken)")
+//                loginManager.logIn([.publishActions], viewController: self) { loginResult in
+//                    switch loginResult {
+//                    case .failed(let error):
+//                        print(error)
+//                    case .cancelled:
+//                        print("User cancelled login.")
+//                    case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+//
+//                        print("Logged in with read permission!, \n grantedPermissions: \(grantedPermissions), \n declinedPermissions: \(declinedPermissions),\n accessToken: \(accessToken)")
+//                    }
+//
+//                }
+//            }
+//        }
         
 //        .custom("user_photos")
 //        loginManager.logIn([.publishActions], viewController: self) { loginResult in
@@ -66,17 +112,24 @@ class HALoginVC: UIViewController, SFSafariViewControllerDelegate {
 //            case .cancelled:
 //                print("User cancelled login.")
 //            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
-//                
+//
 //                print("Logged in with read permission!, \n grantedPermissions: \(grantedPermissions), \n declinedPermissions: \(declinedPermissions),\n accessToken: \(accessToken)")
 //            }
-//            
-//        }
+//
         
 
         
         
         
 
+    @IBAction func TWLogOutClick(_ sender: Any) {
+        let store = Twitter.sharedInstance().sessionStore
+        
+        if let userID = store.session()?.userID {
+            store.logOutUserID(userID)
+            print(store.existingUserSessions())
+        }
+        
     }
     
     @IBAction func FBLogOutClick(_ sender: Any) {
