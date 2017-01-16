9f0446f0dd900399e00226173172d50657a09efa
LONG MA <longma@LONGs-MacBook-Pro.local>
2017-01-16 17:36:43 +0800

    1. Create OC in Swift3 bridging file 2. integrate OC style twitter and Facebook video resumable upload function code

---
+++ PostHelper/PostHelper/Other/PostHelper-Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  PostHelper-Bridging-Header.h
+//  PostHelper
+//
+//  Created by LONG MA on 16/1/17.
+//  Copyright © 2017 HnA. All rights reserved.
+//
+
+#ifndef PostHelper_Bridging_Header_h
+#define PostHelper_Bridging_Header_h
+
+#import "SocialVideoHelper.h"
+
+#endif /* PostHelper_Bridging_Header_h */
